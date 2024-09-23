e9ca610d2ed5fa2941c06b29b652f51ca600e3be
Koni Marti <koni.marti@gmail.com>
2024-09-23 18:20:05 +0200

    markdown: handle email or http values

--- src/cical_print_markdown.c
+++ src/cical_print_markdown.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 
 /*
  *
@@ -39,6 +40,64 @@ markdown_print_indented_char(FILE *f, void *obj, int indent)
 	fprintf(f, "    - %s", p);
 }
 
+typedef void (*handle_prefix)(FILE *f, char *s);
+
+static void
+mailto_handler(FILE *f, char *s)
+{
+	fprintf(f, "<%s>", s + 7);
+}
+
+static void
+link_handler(FILE *f, char *s)
+{
+	fprintf(f, "[link](%s)", s);
+}
+
+struct {
+	char *prefix;
+	handle_prefix handler;
+} value_prefix[] = {
+	{ "mailto:", mailto_handler },
+	{ "http:", link_handler },
+	{ "https:", link_handler },
+};
+
+/* static const char *const time_prefix = "DT"; */
+
+static void
+markdown_print_property_value(FILE *f, struct property p[static 1], int indent)
+{
+	/* check for certain prefixes */
+	size_t value_len = strlen(p->value);
+	for (size_t i = 0; i < 3 /*FIXME: magic nr*/; i++) {
+		size_t len = strlen(value_prefix[i].prefix);
+		if (value_len < len) {
+			continue;
+		}
+		if (strncasecmp(p->value, value_prefix[i].prefix, len) == 0) {
+			value_prefix[i].handler(f, p->value);
+			return;
+		}
+	}
+
+	/* check for certain prefixes */
+	/* TODO: check time implementation
+	size_t name_len = strlen(p->name);
+	if (name_len > strlen(time_prefix)
+		&& strncasecmp(p->name, time_prefix, strlen(time_prefix))) {
+		time_t t = 0;
+		if (parse_rfc5545_time(p->value, &t) != 0) {
+			fprintf(f, "%s", ctime(&t));
+			return;
+		}
+	}
+	*/
+
+	/* at this point, we just print the value */
+	fprintf(f, "%s", p->value);
+}
+
 static void
 markdown_print_params(FILE *f, void *obj, int indent)
 {
@@ -53,7 +112,10 @@ markdown_print_property(FILE *f, void *obj, int indent)
 {
 	struct property *p = obj;
 	if (p->value && strlen(p->value)) {
-		fprintf(f, "- *%s*: %s\n", p->name, p->value);
+		fprintf(f, "- *%s*: ", p->name);
+		markdown_print_property_value(f, p, indent);
+		fprintf(f, "\n");
+
 	} else {
 		fprintf(f, "- *%s*\n", p->name);
 	}
